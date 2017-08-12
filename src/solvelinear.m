e1149c626f79050dd19c1072324c102db17e87bb
Ankit Pati <contact@ankitpati.in>
2017-08-12 10:52:58 +0530

    add two more methods of solving

--- src/solvelinear.m
+++ src/solvelinear.m
@@ -16,9 +16,17 @@ results = [
     -10;
 ];
 
+disp("\nUsing linsolve...");
 opts.TRANSA = false; % TRANSA is buggy in Octave 4.0.0
 x = linsolve(coeffs, results, opts);
+printf("x = %.2f\ny = %.2f\nz = %.2f\n", x(1), x(2), x(3));
 
+disp("\nUsing \\...");
+x = coeffs \ results;
+printf("x = %.2f\ny = %.2f\nz = %.2f\n", x(1), x(2), x(3));
+
+disp("\nUsing inv...");
+x = inv(coeffs) * results;
 printf("x = %.2f\ny = %.2f\nz = %.2f\n", x(1), x(2), x(3));
 
 % end of linsolve.m
