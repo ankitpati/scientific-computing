80088a4b23c91f152b5cb39cfd4e9b103636ce4b
Ankit Pati <contact@ankitpati.in>
2017-08-26 11:08:09 +0530

    keyboard-based debugging

---
+++ src/keyboard-control.m
@@ -0,0 +1,18 @@
+#!/usr/bin/octave -q
+
+% keyboard-control.m
+% Date  : 26 August 2017
+% Author: Ankit Pati
+
+a = 12;
+b =  4;
+
+x = a + b;
+y = a / b;
+
+keyboard("Inspect y; set y to ten> ");
+
+z = a - y + 3;
+printf("Value of z is %d.\n", z);
+
+% end of keyboard-control.m
