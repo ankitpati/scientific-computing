26920fc7158c66faf7ae0db8f061d1618933725b
Ankit Pati <contact@ankitpati.in>
2017-08-26 10:44:46 +0530

    choose a colour from a menu

---
+++ src/colourmenu.m
@@ -0,0 +1,17 @@
+#!/usr/bin/octave -q
+
+% colourmenu.m
+% Date  : 26 August 2017
+% Author: Ankit Pati
+
+colours = {"Red", "Green", "Blue", "Black"};
+
+choice = menu("Choose a Colour", colours);
+
+if choice != 0
+    printf("Chosen colour is %s.\n", colours{choice});
+else
+    disp("No colour chosen.");
+end
+
+% end of colourmenu.m
