792a2b6747564f1d3a4ff274afecfbab9fd09429
Ankit Pati <contact@ankitpati.in>
2017-09-10 12:42:02 +0530

    compact plot properties representation

--- src/lineplots.m
+++ src/lineplots.m
@@ -13,20 +13,11 @@ ylabel('y');
 
 x = 0 : 0.01 : 10;
 
-plot(x,  2 .* x -  9, 'color', 'blue'  , 'linewidth', 2, 'linestyle', '-' ,
-                                                            'DisplayName', '1');
-
-plot(x,  5 .* x + 12, 'color', 'yellow', 'linewidth', 4, 'linestyle', '-' ,
-                                                            'DisplayName', '2');
-
-plot(x,  9 .* x +  3, 'color', 'green' , 'linewidth', 2, 'linestyle', '--',
-                                                            'DisplayName', '3');
-
-plot(x, 11 .* x - 11, 'color', 'red'   , 'linewidth', 4, 'linestyle', ':' ,
-                                                            'DisplayName', '4');
-
-plot(x, 13 .* x +  4, 'color', 'black' , 'linewidth', 2, 'linestyle', '-.',
-                                                            'DisplayName', '5');
+plot(x,  2 .* x -  9, 'linewidth', 2, '-b' , 'DisplayName', '1');
+plot(x,  5 .* x + 12, 'linewidth', 4, '-y' , 'DisplayName', '2');
+plot(x,  9 .* x +  3, 'linewidth', 2, '--g', 'DisplayName', '3');
+plot(x, 11 .* x - 11, 'linewidth', 4, ':r' , 'DisplayName', '4');
+plot(x, 13 .* x +  4, 'linewidth', 2, '-.k', 'DisplayName', '5');
 
 legend('show');
 
