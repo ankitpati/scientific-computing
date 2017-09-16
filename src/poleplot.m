9121c53de1413b076bdced43c9524e78a597539c
Ankit Pati <contact@ankitpati.in>
2017-09-16 11:33:56 +0530

    polar plotting

---
+++ src/poleplot.m
@@ -0,0 +1,24 @@
+#!/usr/bin/octave -q
+
+% poleplot.m
+% Date  : 16 September 2017
+% Author: Ankit Pati
+
+hold on;
+grid on;
+
+title('Polar Plots');
+xlabel('x');
+ylabel('y');
+
+t = linspace(-pi/2, pi/2);
+f = sin(4*t);
+
+polar(t, f, ';sin 4t;');
+
+legend('show');
+
+drawnow();
+input("Press enter key to terminate...");
+
+% end of poleplot.m
