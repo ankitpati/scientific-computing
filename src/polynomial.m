695b872873c848f24f2e71417f3c1405b34cbafd
Ankit Pati <contact@ankitpati.in>
2017-09-23 09:27:36 +0530

    use polyout to display polynomials

--- src/polynomial.m
+++ src/polynomial.m
@@ -12,47 +12,47 @@ val = polyval(p, 9);    % p(9)
 printf("p(s) at s = 9 is %.2f.\n\n", val);
 
 sum = p + q;
-disp("Sum = "), disp(sum), disp("");
+disp("Sum = "), polyout(sum), disp("");
 
 dif = p - q;
-disp("Difference = "), disp(dif), disp("");
+disp("Difference = "), polyout(dif), disp("");
 
 mul = conv(p, q);
 %mul = p * transpose(q);    % identical to the above
-disp("Product = "), disp(q), disp("");
+disp("Product = "), polyout(q), disp("");
 
-[quo, rem] = deconv(p, q_); % 0-padded polynomials don’t pass through filter
-disp("Quotient  = "), disp(quo),
-disp("Remainder = "), disp(rem), disp("");
+[quo, rem] = deconv(p, polyreduce(q)); % 0-padded polynomials don’t pass through filter
+disp("Quotient  = "), polyout(quo),
+disp("Remainder = "), polyout(rem), disp("");
 
 der = polyder(p);
-disp("Derivative of p = "), disp(der), disp("");
+disp("Derivative of p = "), polyout(der), disp("");
 
 int = polyint(p, 4);        % constant of integration, k = 4
-disp("Integral of p = "), disp(int), disp("");
+disp("Integral of p = "), polyout(int), disp("");
 
 int = polyint(q, 4);
-disp("Integral of q = "), disp(int), disp("");
+disp("Integral of q = "), polyout(int), disp("");
 
 rot = roots(p);
-disp("Roots of p = "), disp(rot), disp("");
+disp("Roots of p = "), polyout(rot), disp("");
 
 pwr = poly([-1, -2]);       % finds polynomial whose roots are in given array
-disp("Polynomial with roots -1, -2 = "), disp(pwr), disp("");
+disp("Polynomial with roots -1, -2 = "), polyout(pwr), disp("");
 
 A = [
     0, 1;
     2, 3;
 ];
 chr = poly(A);  % finds characteristic polynomial of A, which is, [1, -3, -2]
-                % that translates to s² - 3s -2
-disp("Characteristic Polynomial of A = "), disp(chr), disp("");
+                % that translates to s² - 3s - 2
+disp("Characteristic Polynomial of A = "), polyout(chr), disp("");
 
 X = [  2,   5,   7];
 Y = [ 24, 156, 384];
 N = 3;
 pft = polyfit(X, Y, N);
-disp("Polyfit for X and Y in N dimensions = "), disp(pft), disp("");
+disp("Polyfit for X and Y in N dimensions = "), polyout(pft), disp("");
 
 Z = [4, 2, 3];  % Z(X) = 4X² + 2X + 3
 X = [           % must be a square matrix
