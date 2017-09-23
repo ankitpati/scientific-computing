57baa571ee7c179daea370cfca6a8a4d3571979f
Ankit Pati <contact@ankitpati.in>
2017-09-23 09:28:11 +0530

    remove opts.TRANSA workaround for Octave 4.0

--- src/solvelinear.m
+++ src/solvelinear.m
@@ -17,8 +17,7 @@ results = [
 ];
 
 disp("\nUsing linsolve...");
-opts.TRANSA = false; % TRANSA is buggy in Octave 4.0.0
-x = linsolve(coeffs, results, opts);
+x = linsolve(coeffs, results);
 printf("x = %.2f\ny = %.2f\nz = %.2f\n", x(1), x(2), x(3));
 
 disp("\nUsing \\...");
