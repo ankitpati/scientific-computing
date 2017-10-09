b68a9a5b03e1192d49ea9cd8afdb28aa25e40d7b
Ankit Pati <contact@ankitpati.in>
2017-10-09 07:28:52 +0530

    using subplots, instead of breaking for user input

--- src/logplot.m
+++ src/logplot.m
@@ -6,26 +6,22 @@
 
 grid on;
 
-title('Logarithmic Plots');
-xlabel('x');
-ylabel('y');
-
 a = [2 + i, 4 + 6i, -2i, 4, 5];
 x = exp(-a);
 y = a.^2;
 
+subplot(311);
 semilogx(x, y, 'linewidth', 2, '-c', 'DisplayName', 'logarithmic x plot');
 legend('show');
-drawnow();
-input("Press enter to continue...");
 
+subplot(312);
 semilogy(x, y, 'linewidth', 2, '-m', 'DisplayName', 'logarithmic y plot');
 legend('show');
-drawnow();
-input("Press enter to continue...");
 
+subplot(313);
 loglog  (x, y, 'linewidth', 2, '-y', 'DisplayName', 'logarithmic plot');
 legend('show');
+
 drawnow();
 input("Press enter to terminate...");
 
