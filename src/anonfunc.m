535f77a2d157d1e2fc08fa9b85ce2a06b98de8bf
Ankit Pati <contact@ankitpati.in>
2017-10-09 07:07:56 +0530

    anonymous function

---
+++ src/anonfunc.m
@@ -0,0 +1,10 @@
+#!/usr/bin/octave -q
+
+% anonfunc.m
+% Date  : 23 September 2017
+% Author: Ankit Pati
+
+f = @(x) x.^2 + 2.*x + 3; % f(x) = x² + 2x + 3
+disp(["f(5) = " num2str(f(5)) "\n"]);
+
+% end of anonfunc.m
