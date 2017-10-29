940163d8be4ef673f3b9e784b5081919b1dce5b8
Ankit Pati <contact@ankitpati.in>
2017-10-29 19:11:27 +0530

    fix complex plots

--- src/cmplxplot.m
+++ src/cmplxplot.m
@@ -16,9 +16,11 @@ y = [ 9  8  7 10  6];
 a = [2 + i, 4 + 6i, -2i, 4, 5];
 
 plot(x, y, 'linewidth', 4, '-r', 'DisplayName', 'x vs. y');
-plot(x,    'linewidth', 2, '-g', 'DisplayName', 'indices vs. values for x');
-plot(a,    'linewidth', 2, '-b', 'DisplayName', 'imaginary plot');
+plot(   x, 'linewidth', 2, '-g', 'DisplayName', 'indices vs. values for x');
+plot(   a, 'linewidth', 2, '-b', 'DisplayName', 'imaginary plot');
 plot(a, y, 'linewidth', 2, '-k', 'DisplayName', 'imaginary vs. real');
+
+plot3(x, a, 'linewidth', 2, '-c', 'DisplayName', 'real vs. real-part vs. imag');
 
 line(x, y, 'linewidth', 2, 'color', 'yellow', 'linestyle', '--',
                                  'DisplayName', 'line(x, y)');
