117f76a07222eb002478418ce43cda0ccd854274
Ankit Pati <contact@ankitpati.in>
2017-10-29 19:22:47 +0530

    reverse a number of 3 digits or less

---
+++ src/revnum.m
@@ -0,0 +1,30 @@
+#!/usr/bin/octave -q
+
+% revnum.m
+% Date  : 28 October 2017
+% Author: Ankit Pati
+
+num = input("Enter a number: ", "s");
+
+try
+    if length(num) > 3
+        error("Number larger than 3 digits.");
+    end
+
+    rev = fliplr(num);
+    disp(rev);
+
+catch err
+    disp(err.message);
+
+end
+
+% end of revnum.m
+
+%{
+Enter a number: 435
+534
+
+Enter a number: 1234
+Number larger than 3 digits.
+%}
